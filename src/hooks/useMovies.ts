bc5fea8ee17938efda7f079f13e5c1847b45e5a5
Omar-Alissa <omarissa179@gmail.com>
2026-05-12 14:56:44 +0200

    Feat: add custom hook useMovies for fetching popular movies

---
+++ src/hooks/useMovies.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { fetchPopularMovies } from '../api/tmdb'
+import type { Movie } from "../types/Movie.ts"
+
+
+const useMovies = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+
+    const controller = new AbortController();
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetchPopularMovies(controller.signal);
+        setMovies(response.results);
+      } catch (err: any) {
+        if (err.name === 'AbortError') {
+          console.log('Fetch aborted safely');
+        } else {
+          setError(err.message);
+        }
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+    return () => controller.abort();
+
+  }, [])
+  return { movies, loading, error }
+}
+
+export default useMovies;
